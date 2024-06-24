6bdf85e97e9a653c650f9ba663d533486420b806
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-24 11:29:23 +0100

    d1e1 with notes

--- exercises/counter.js
+++ exercises/counter.js
@@ -1,7 +1,25 @@
 // Make your Counter class here
 
+class Counter {
+    constructor(count) {
+        this.count = 0
+    }
+}
+
 // Replace `null` with an _instance_ of your counter
-const counterInstance = null
+const counterInstance = new Counter()
 
 // Please don't change the lines below!
 export { Counter, counterInstance }
+
+/*
+- create a new class called `Counter`: Make sure your class name has a capital `C`
+- Give it a constructor that initialises `count` to `0`
+- Assign the variable `counterInstance` to an instance of your counter
+      class.
+
+      Tech Docs:
+      To create an instance from this blueprint (class), we use the "new" keyword 
+
+The count didn't work initially as I didn't use "this."       
+*/ 
